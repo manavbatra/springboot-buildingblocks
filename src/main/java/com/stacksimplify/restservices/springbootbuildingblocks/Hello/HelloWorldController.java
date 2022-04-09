27b490f629273716ea7876017566f81976c4bd1d
Manav Batra <manavbatra26@gmail.com>
2022-04-09 18:39:30 -0400

    Hello World

---
+++ src/main/java/com/stacksimplify/restservices/springbootbuildingblocks/Hello/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.stacksimplify.restservices.springbootbuildingblocks.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+//	@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld")
+	public String helloworld() {
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Manav", "Batra", "Toronto");
+	}
+
+}
